099d23d8b63e976645c164f4becac1092a8959fc
isaaki <xisaaki@proton.me>
2025-10-26 10:54:04 +0100

    small fix to localstorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import SideBar from "./SideBar";
 import MainPage from "./MainPage";
+
+import type { User } from "./types";
+
 import "./assets/fontawesome-free/css/all.css";
 import "./reset.css";
 import "./App.css";
@@ -9,10 +12,11 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:8080/api/user/2")
       .then((res) => res.json())
-      .then((data) => {
-        localStorage.setItem("userID", data.ID);
+      .then((data: User) => {
+        localStorage.setItem("userID", String(data.ID));
         localStorage.setItem("username", data.Username);
         localStorage.setItem("name", data.Name);
+        localStorage.setItem("profilePicUrl", data.ProfilePicUrl);
       })
       .catch(console.error);
   }, []);
